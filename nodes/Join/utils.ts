32f1dc5da9f9a8da0a91ca51037219a0a8fa9549
Noah Halstead <nhalstead@users.noreply.github.com>
2026-01-09 13:14:56 -0500

    Update utils.ts

--- nodes/Join/utils.ts
+++ nodes/Join/utils.ts
@@ -3,7 +3,7 @@ import type { INodeParameters } from 'n8n-workflow';
 
 /**
  * Copied from packages/nodes-base/nodes/Merge/v3/helpers/utils.ts
- * Used a a template literal to generate the inputs for the node
+ * Used a template literal to generate the inputs for the node
  *
  * @param parameters
  */
